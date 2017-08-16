6dd75b5c9b19d45464c16325fe4907046ad3299a
Agustina Hinojosa <agustina.hinojosa@gmail.com>
2017-08-15 20:10:54 -0700

    changes comments and renames a var

--- js/calculations.js
+++ js/calculations.js
@@ -5,13 +5,13 @@ function final_armor(base, agi, tower, aura_armor, reduction) {
 };
 
 function calc_level_stats(hero, level) {
-  // should be an array of [str,agi,int,str_gain,agi_gain,int_gain]
-  var these_stats = find_hero_base_stats(hero);
-  console.log("calc base "+ these_stats);
+  // base_stats = [str,agi,int,
+  //                str_gain,agi_gain,int_gain]
+  var base_stats = find_hero_base_stats(hero);
   var new_stats = [
-    these_stats[0]+(these_stats[4]*level),
-    these_stats[1]+(these_stats[5]*level),
-    these_stats[2]+(these_stats[6]*level)
+    base_stats[0]+(base_stats[3]*level),
+    base_stats[1]+(base_stats[4]*level),
+    base_stats[2]+(base_stats[5]*level)
   ];
   return new_stats;
 };
@@ -22,7 +22,6 @@ function find_hero_base_stats(heroNum) {
   for (var hero in HEROES.DOTAHeroes) {
     HEROES.DOTAHeroes[hero].HeroID == heroNum ? heroObj = HEROES.DOTAHeroes[hero] : null;
   };
-  console.log(heroObj || "nein");
   base_stats = [
     parseInt(heroObj.AttributeBaseStrength, 10),
     parseInt(heroObj.AttributeBaseAgility, 10),
