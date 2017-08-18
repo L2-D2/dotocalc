ec0594de3f5f38e82c796f901b7369a3dc95ef07
Agustina Hinojosa <omnilemm@gmail.com>
2017-08-18 00:17:33 -0700

    pops find_hero into its own function; adds vars for calc_dps

--- js/calculations.js
+++ js/calculations.js
@@ -1,4 +1,4 @@
-function final_armor(base, agi, tower, aura_armor, reduction) {
+function calc_final_armor(base, agi, tower, aura_armor, reduction) {
   var bonus_armor_tower = tower == 0 ? 0 : tower == 1 ? 1 : 3;
   var armor = base + agi/7 + bonus_armor_tower + aura_armor - reduction;
   return armor;
@@ -16,12 +16,39 @@ function calc_level_stats(hero, level) {
   return new_stats;
 };
 
-function find_hero_base_stats(heroNum) {
-  var base_stats;
+function calc_dps(whom) {
+//  DPS =
+  //  (((main damage × (1 + Σ percentage bonus damage) + flat bonus damage)
+  //    × critical strike multiplier - blocked damage )
+  //    × armor value multiplier × armor type multiplier
+  //    × general damage multipliers) x attacks per second
+
+  var heroObj = find_hero(
+    $(whom + ", .heroSelect" ).val()
+  );
+  var dmg_main;
+  var dmg_bonus_percent;
+  var dmg_bonus_flat;
+  var scalar_crit;
+  var dmg_blocked;
+  var scalar_armor
+  var scalar_armor_type;
+  var scalar_general;
+  
+}
+
+function find_hero(id) {
+  // Should return the object of the hero given the id
   var heroObj;
   for (var hero in HEROES.DOTAHeroes) {
-    HEROES.DOTAHeroes[hero].HeroID == heroNum ? heroObj = HEROES.DOTAHeroes[hero] : null;
+    HEROES.DOTAHeroes[hero].HeroID == id ? heroObj = HEROES.DOTAHeroes[hero] : null;
   };
+  return heroObj;
+}
+
+function find_hero_base_stats(heroNum) {
+  var base_stats;
+  var heroObj = find_hero(heroNum);
   base_stats = [
     parseInt(heroObj.AttributeBaseStrength, 10),
     parseInt(heroObj.AttributeBaseAgility, 10),
