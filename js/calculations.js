a58fde2c41389d650322afc30cf1c1514f40702e
Agustina Hinojosa <omnilemm@gmail.com>
2017-08-18 16:15:38 -0700

    moves finding information to discoveries file

--- js/calculations.js
+++ js/calculations.js
@@ -89,26 +89,3 @@ function calc_dmg_base_avg (min, max) {
   for (let i = 0; i < rand_int; i++) { valSum += (Math.random() * (max - min) + avg) };
   return valSum / (parseFloat(rand_int));
 };
-
-function find_hero(id) {
-  // Should return the object of the hero given the id
-  var heroObj;
-  for (var hero in HEROES.DOTAHeroes) {
-    HEROES.DOTAHeroes[hero].HeroID == id ? heroObj = HEROES.DOTAHeroes[hero] : null;
-  };
-  return heroObj;
-}
-
-function find_hero_base_stats(heroNum) {
-  var base_stats;
-  var heroObj = find_hero(heroNum);
-  base_stats = [
-    parseInt(heroObj.AttributeBaseStrength, 10),
-    parseInt(heroObj.AttributeBaseAgility, 10),
-    parseInt(heroObj.AttributeBaseIntelligence, 10),
-    parseFloat(heroObj.AttributeStrengthGain, 10),
-    parseFloat(heroObj.AttributeAgilityGain, 10),
-    parseFloat(heroObj.AttributeIntelligenceGain, 10)
-  ];
-  return base_stats;
-};
