b5c90dd85a880a55454cdf91958c1f0cf79683a4
Agustina Hinojosa <omnilemm@gmail.com>
2017-08-18 00:49:06 -0700

    adds ATTR_DICT to help reading from JSON

--- js/ready.js
+++ js/ready.js
@@ -1,6 +1,11 @@
 var HEROES;
 var ITEMS;
 const ATTRS = ["str", "agi", "int"];
+const ATTR_DICT = {
+  DOTA_ATTRIBUTE_STRENGTH: ATTRS[0],
+  DOTA_ATTRIBUTE_AGILITY: ATTRS[1],
+  DOTA_ATTRIBUTE_INTELLEGENCE: ATTRS[2]
+};
 
 function makeHeroOptions() {
   for (var key in HEROES.DOTAHeroes) {
