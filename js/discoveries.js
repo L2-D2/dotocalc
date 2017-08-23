a20cbd278efa17bc521dbc0182ace43f7eaacc93
Agustina Hinojosa <omnilemm@gmail.com>
2017-08-22 20:27:39 -0700

    renames simple funcs to yank; adds yank_current_attrs

--- js/discoveries.js
+++ js/discoveries.js
@@ -1,4 +1,4 @@
-function find_hero(id) {
+function yank_hero_obj(id) {
   // Should return the object of the hero given the id
   var heroObj;
   for (var hero in HEROES.DOTAHeroes) {
@@ -9,7 +9,7 @@ function find_hero(id) {
 
 function find_hero_base_stats(heroNum) {
   var base_stats;
-  var heroObj = find_hero(heroNum);
+  var heroObj = yank_hero_obj(heroNum);
   base_stats = [
     parseInt(heroObj.AttributeBaseStrength, 10),
     parseInt(heroObj.AttributeBaseAgility, 10),
@@ -31,14 +31,26 @@ function yank_item_special(id) {
   return bons
 };
 
+function yank_current_attrs(whom) {
+  let attrs = new Array;
+  var spot = "#"+whom+"AttrSpot";
+  ATTRS.forEach( function(attr, i) {
+    attrs[i] = parseFloat( $(spot).find("p."+attr).text() );
+    // should do in [str, agi, int] order
+  });
+  return attrs;
+};
+
 function find_items_special(whom) {
   let items_special = new Object;
   let special_array = new Array;
   let itemIDs = new Array;
+
   // itemIDs: the 6 items in an array as item.IDs
   $("."+whom+".itemSpot").find(".itemDrop").map( function() {
     itemIDs.push( $().add(this).val() );
   });
+
   // items_special = { item.ID: { count: [1..6], special: [special_array] }, otheritem.ID: {..}, ..}
   itemIDs.forEach( function(id) {
     items_special.hasOwnProperty(id) ? items_special[id].count += 1 : items_special[id] = {
@@ -46,6 +58,7 @@ function find_items_special(whom) {
       "special": yank_item_special(id)
     };
   });
+
   return items_special;
 };
 
