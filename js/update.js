c9cb9e9d5bb425325f5e1a8b1d645ee2c9a88cff
Agustina Hinojosa <omnilemm@gmail.com>
2017-08-23 07:23:42 -0700

    moves updating things to single command; also TEMPLATE LITERALS

--- js/update.js
+++ js/update.js
@@ -1,34 +1,44 @@
 function updateAttrs(whom) {
-  var spot = "#"+whom+"AttrSpot";
-  var hero = $(whom + ", .heroSelect" ).val();
-  var lvl = parseInt($("#"+whom+"Level").text());
+  var spot = `#${whom}AttrSpot`;
+  var hero = $(`.${whom}.heroSelect`).val();
+  var lvl = parseInt($(`#${whom}Level`).text());
   var stats = calc_level_stats(hero, lvl);
   // console.log(spot)
   stats.forEach(function(val, i) {
     var cleanVal = val.toFixed(1);
-    $(spot).find("p."+ATTRS[i]).text(cleanVal);
+    $(spot).find(`p.${ATTRS[i]}`).text(cleanVal);
   });
 };
 
 function updateDPS(whom, parent) {
-  var spot = "#"+whom+"DPS";
+  var spot = `#${whom}DPS`;
   var dps = calc_dps(whom, parent);
   $(spot).text(dps);
 }
 
+function updateArmor(whom) {
+  let armorClass = ARMORTYPES[1];
+  $(`p.${whom}.armorText`).text(`${armorClass} Armor`);
+  $(`.${whom}.armorVal`).text()
+}
+
+function updateEverything(whom, event) {
+  updateAttrs(whom);
+  updateDPS(whom, event);
+  updateArmor(whom);
+}
+
 window.onload = function() {
-  ["you", "them"].forEach( function(i) {
-    $(".heroSelect."+ i).on("selectmenuchange", function(e) {
-      updateAttrs(i);
-      updateDPS(i, e);
+  PLAYERS.forEach( function(i) {
+    $(`.heroSelect.${i}`).on("selectmenuchange", function(e) {
+      updateEverything(i,e);
     });
-    $("#"+i+"LevelSlider").on("slide", function(event, ui) {
-      $("#"+i+"Level").text(ui.value);
-      updateAttrs(i, ui.value);
-      updateDPS(i, event);
+    $(`#${i}LevelSlider`).on("slide", function(e, ui) {
+      $(`#${i}Level`).text(ui.value);
+      updateEverything(i,e);
     });
-    $(".itemDrop."+i).on("selectmenuchange", function(e) {
-      updateDPS(i,e)
+    $(`.${i}.itemDrop`).on("selectmenuchange", function(e) {
+      updateEverything(i,e);
     });
     updateAttrs(i);
   });
