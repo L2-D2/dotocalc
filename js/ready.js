c19923283e60e98303dd3e408ccf8d3de97bd692
Agustina Hinojosa <omnilemm@gmail.com>
2017-08-23 17:08:26 -0700

    moves hero picker up a row; adds values to tower radios

--- js/ready.js
+++ js/ready.js
@@ -35,26 +35,36 @@ function makeSkeleton(whom) {
   let whomPossessive = PLAYER_DICT[whom].possessive;
   let whomPossessiveSafe = PLAYER_DICT[whom].possessiveSafe;
   $("#main").append(`<div id="${whom}" class="full-hero container-fluid col-lg-6">`);
-  $(`#${whom}`)
-    .append(`<span><h2>Pick ${PLAYER_DICT[whom].pickHero}!</h2></span>`)
-    .append(`<span class="row ${whom} topRow"></span>`)
-    .append(`<span><div id="${whom}LevelSlider" class="${whom} lvlSlider"></div></span>`)
-    .append(`<span><h3>Pick ${whomPossessive} Items!</h3></span>`)
-    .append(`<span class="${whom} itemSpot"></span>`)
-    .append(`<span>Pocket Riki? <input type="checkbox"></input></span>`)
-    .append(`<span class="row ${whom} dpsRow vertAlign justify-content-center">`);
-  $(`.${whom}.topRow`) // topRow
-    .append(`<div class="col">
-      <div class="row container-fluid ${whom} armorPlusLevel">
-        <div class="col vertAlign"><p>${whomPossessive} Level</p></div>
-        <div class="col">
-        <p class="box " id="${whom}Level">1</p>
+  $(`#${whom}`).append(
+      `<span class="row">
+        <div class="col vertAlign justify-content-center">
+          <h2>Pick ${PLAYER_DICT[whom].pickHero}!</h2>
         </div>
-        <span class="w-100"></span>
-      </div>
-    </div>`)
-    .append(`<div class="col vertAlign justify-content-center"><select class="${whom} heroSelect"></select></div>`)
-    .append(`<div class="col vertAlign" id="${whom}AttrSpot"></div>`);  // !topRow
+        <div class="col vertAlign justify-content-center">
+          <select class="${whom} heroSelect"></select>
+        </div>
+      </span>`)
+      .append(
+        `<span =class="row">
+          <div class="col">
+            <div class="row container-fluid ${whom} armorPlusLevel">
+              <div class="col vertAlign"><p>${whomPossessive} Level</p></div>
+              <div class="col">
+                <p class="box " id="${whom}Level">1</p>
+              </div>
+              <span class="w-100"></span>
+            </div>
+          </div>
+          <div class="col vertAlign" id="${whom}AttrSpot"></div>
+        </span>`)
+      .append(
+        `<span>
+          <div id="${whom}LevelSlider" class="${whom} lvlSlider"></div>
+        </span>`)
+      .append(`<span><h3>Pick ${whomPossessive} Items!</h3></span>`)
+      .append(`<span class="${whom} itemSpot"></span>`)
+      .append(`<span>Pocket Riki? <input type="checkbox"></input></span>`)
+      .append(`<span class="row ${whom} dpsRow vertAlign justify-content-center">`);
   $(`.${whom}.dpsRow`) // dps row
     .append(`<div class="col"><span><button onclick='updateDPS("${whom}", "${whomPossessiveSafe}Button")'>DPS</button></span></div>`)
     .append(`<div class="col"><div class="box"><p id="${whom}DPS">Over 9000!</p></div></div>`)
@@ -64,7 +74,7 @@ function makeSkeleton(whom) {
     .append(`<div class="col vertAlign justify-content-center ${whom} towerChecks"></div>`);
   [0,1,2,3].forEach(function(i) {
     let words = ( i==0 ? "No" : `T${i}` );
-    $(`.${whom}.towerChecks`).append(`<span class="t${i}Check">${words}?<input name="${whom}Tower" type="radio"></input></span>`);
+    $(`.${whom}.towerChecks`).append(`<span class="t${i}Check">${words}?<input name="${whom}Tower" value="${i}" type="radio"></input></span>`);
   });
 };
 
